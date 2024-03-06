4dedaeba62dc1fc2f0363cd6290a1b257d02c843
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-03-06 23:21:17 +0000

    u

--- index.php
+++ index.php
@@ -7,4 +7,4 @@
   </head>
   <body>
     <?php echo '<h1>Hello, World! in PHP</h1>'; ?>
-    <?php echo '<p>This is my first web page created in PHP.</p>';
+    <?php echo '<p>This is Niko's first web page created in PHP.</p>';
